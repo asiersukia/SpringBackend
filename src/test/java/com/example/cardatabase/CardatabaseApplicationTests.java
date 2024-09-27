7604a6007edd6969421551d8db535239a71649fe
asiersukia <45184878+asiersukia@users.noreply.github.com>
2024-09-27 18:08:53 +0200

    Tests

--- src/test/java/com/example/cardatabase/CardatabaseApplicationTests.java
+++ src/test/java/com/example/cardatabase/CardatabaseApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.cardatabase;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.cardatabase.web.CarController;
 
 @SpringBootTest
 class CardatabaseApplicationTests {
-
+	@Autowired
+    private CarController controller;
 	@Test
+	@DisplayName("First example test case")
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
